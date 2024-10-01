41ccf34be157fca283aa64c41714e2c3520cd623
nhsahu <nsahu87654@gmail.com>
2024-10-01 15:45:42 +0530

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,15 +1,2 @@
-shell scripting
-
 #!/bin/bash
-echo"First line of jenkins code"
-
-echo"time is"
-time
-date
-ps
-
-sh/bash <script_name>
-
-<script_name>.py
-
-javac <script_name>.java
+echo"hello world"
